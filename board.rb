42d2ed447ad94f5469473cadbd60c61307d1b825
App Academy Student <appacademy@Apps-Mac-mini-9.local>
2015-10-05 14:20:20 -0400

    Handle position errors

--- board.rb
+++ board.rb
@@ -5,13 +5,34 @@ class Board
   def initialize
     @grid = Array.new(8) { Array.new(8) }
     place_pieces
+  end
+
+  def [](pos)
+    x,y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, value)
+    x,y = pos
+    grid[x][y] = value
+  end
+  def place_pieces
+    rows = [0,1,6,7]
+    rows.each do |idx|
+      fill_row(grid[idx])
+    end
 
   end
 
-  def place_pieces
-    grid[0].map! do |square|
+  def fill_row(row)
+    row.map! do |square|
       square = Piece.new
     end
   end
+
+  def move(start,end_pos)
+    begin
+
+
 
 end
