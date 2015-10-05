6b2cd300fa453deb9e8fe70abf6ac0c5ac436d0f
Patricia and John <patandjohn@appacademy.io>
2015-10-05 14:47:53 -0400

    Get user input

--- display.rb
+++ display.rb
@@ -1,8 +1,22 @@
 require 'colorize'
+require 'io/console'
 
 class Display
 
   attr_accessor :board, :game
+
+  KEYMAP = {
+    "w" => :up,
+    "a" => :left,
+    "s" => :down,
+    "d" => :right,
+    "\e[A" => :up,
+    "\e[B" => :down,
+    "\e[C" => :right,
+    "\e[D" => :left,
+    " " => :select,
+    "\r" => :select
+  }
 
   def initialize(board,game)
 
@@ -11,4 +25,16 @@ class Display
 
   end
 
+  def get_input
+    input = STDIN.getc
+    raise CursorError.new("Invalid key") if !KEYMAP.include?(input)
+  rescue CursorError => e
+    puts e.message
+    retry
+  end
+
+
+end
+
+class CursorError < StandardError
 end
